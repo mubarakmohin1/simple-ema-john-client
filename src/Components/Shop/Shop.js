a8a3941a304d85c6eccd2cb0574f0864ca430494
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-03-23 19:32:30 +0600

    second commit

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import {addToDb, getStoreCart} from '../../utilities/fakedb';
 import './Shop.css'
 
 const Shop = () => {
@@ -10,10 +11,26 @@ const Shop = () => {
         fetch('products.json').then(res => res.json()).then(data => setProducts(data))
     },[])
 
+useEffect(() =>{
+    const storedCart = getStoreCart();
+    const saveCart = [];
+    for(const id in storedCart){
+        const addedProduct = products.find(product => product.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            saveCart.push(addedProduct);
+            
+        }
+    }
+    setCart(saveCart);
+},[products])
+
     const addTocart = (product) =>{
         console.log(product)
         const newCart = [...cart,product];
         setCart(newCart);
+        addToDb(product.id);
 
     }
 
